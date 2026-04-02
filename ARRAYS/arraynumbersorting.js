c814a8050b02255b4ae4a19b40f765d9f229e922
Prathish S <prathish472@gmail.com>
2026-04-02 08:49:38 +0530

    Create arraynumbersorting.js

---
+++ ARRAYS/arraynumbersorting.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+    
+
+    <script src="new.js"></script>
+    
+</body>
+</html>
+=====================================
+  
+let am = [88.55,12,96,7,1,3]
+let g = am.sort()((a,b)=>a-b)
+console.log(g)
+
+// for ascending order
+
+
+
+let am = [88.55,12,96,7,1,3]
+let g = am.sort().reverse((a,b)=>a-b)
+console.log(g)
+
+// for descending order
