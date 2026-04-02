b781ce507f0ab554e28015b96da5a204e09c6eeb
Prathish S <prathish472@gmail.com>
2026-04-02 08:46:26 +0530

    Create arrayloop.js

---
+++ ARRAYS/arrayloop.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+    
+
+    <script src="new.js"></script>
+    
+</body>
+</html>
+=====================================
+  
+var fruit = ["apple","grape","banana","sugarcane","kiwi","fig"]
+
+for(i=0;i<=fruit.length-1;i++)
+{
+    console.log(fruit[i])
+}
