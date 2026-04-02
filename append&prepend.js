577b41146816758ac775a8b5c966fc057aea8bf6
Prathish S <prathish472@gmail.com>
2026-04-02 08:44:51 +0530

    Create append&prepend.js

---
+++ append&prepend.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+
+    <script src="new.js"></script>
+    
+</body>
+</html>
+
+-----------------------------------------------
+<div>
+    <h1>world</h1>
+</div>
+
+<script>
+    var hi = document.querySelector("div")
+  hi.append("hello append")
+  hi.prepend("hello prepend")
+</script>
