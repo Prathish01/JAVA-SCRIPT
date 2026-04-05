6c7c534a19ebdb3009948f17ab9909b54430ec3e
Prathish S <prathish472@gmail.com>
2026-04-05 08:34:01 +0530

    Create domsub.js

---
+++ domsub.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <script src="new.js"></script>
+    
+</body>
+</html>
+
+===================================================================
+
+ value 1:<input type="number" id="demo1" value="26"> <br>
+  value 2:<input type="number" id="demo2" value="4"> <br>
+  result : <input type="number" id="result"> <br>
+  <button onclick="sub()">subtract</button>
+
+
+
+
+  <script>
+    var num1 = document.getElementById("demo1")
+    var num2 = document.getElementById("demo2")
+    var result = document.getElementById("result")
+
+    var one = Number(num1.value)
+    var two = Number(num2.value)
+console.log(one)
+console.log(two)
+    function sub()
+    {
+        sub = one - two
+        console.log(sub)
+    }
+
+  </script>
