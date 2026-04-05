b4f33e1977d04cb648a124d64e1c8aa4b77b5844
Prathish S <prathish472@gmail.com>
2026-04-05 08:45:03 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+    
+    
+    <script src="new.js"></script>
+    
+</body>
+</html>
+
+==================================
+
+let person =
+{
+    name : "murali,",
+    age : 25,
+    FavoriteFood: "briyani",
+    hei : () => console.log("hello  murali")
+}
+
+console.log(person.hei())
