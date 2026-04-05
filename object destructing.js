ec3ce57061dfed82bea9ed26d68c694793072087
Prathish S <prathish472@gmail.com>
2026-04-05 08:43:46 +0530

    Create object destructing.js

---
+++ object destructing.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+    
+    
+    <script src="new.js"></script>
+    
+</body>
+</html>
+
+===================================================
+
+let person = {
+    name: "murali",
+    age: 25,
+    FavoriteFood: "briyani",
+};
+
+let { name, age, FavoriteFood } = person;
+
+console.log(name);        // murali
+console.log(age);         // 25
+console.log(FavoriteFood); // briyani
