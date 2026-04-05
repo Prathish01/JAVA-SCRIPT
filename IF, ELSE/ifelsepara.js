57fded695bffc30386b7cd0c9e473cc8ae7627f6
Prathish S <prathish472@gmail.com>
2026-04-05 08:36:22 +0530

    Create ifelsepara.js

---
+++ IF, ELSE/ifelsepara.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <script src="new.js"></script>
+    
+</body>
+</html>
+  
+=======================================
+  
+function hi(climate)
+{
+if(climate == "spring")
+{
+    console.log("enjoy the booming flowers")
+}
+else if(climate == "summer")
+{
+    console.log("enjoy with the sun")
+}
+else if(climate == "autumn")
+{
+    console.log("Admire the colorful flowers")
+}
+else{
+    console.log("enjoy the rain")
+}
+}
+hi("summer")
